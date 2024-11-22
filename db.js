90f24d874f584b1b1369dc49b416baec65068894
Akash Khedar <akashkhedar262@gmail.com>
2024-11-22 18:55:03 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/urlshortner";
+const mongoURI = process.env.MONGODB ?? "mongodb://localhost:27017/urlshortner";
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI)
